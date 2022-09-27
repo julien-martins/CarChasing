0af323322f6e40b19562d0d2e272a398e820c16e
julien-martins <39356114+julien-martins@users.noreply.github.com>
2022-09-27 20:56:16 +0200

    Drift System

--- Assets/Scripts/Movements/PlayerMovement.cs
+++ Assets/Scripts/Movements/PlayerMovement.cs
@@ -1,11 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using Newtonsoft.Json;
+using UnityEditor.TextCore.Text;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerMovement : MonoBehaviour
 {
+    public float MoveSpeed = 2.0f;
+    public float MaxSpeed = 4.0f;
+
+    public float SteerAngle = 50.0f;
+
+    public float Drag = 0.98f;
+
+    public float Traction = 10.0f;
+    public bool Drifting = false;
+
+    public TrailRenderer left;
+    public TrailRenderer right;
+
     private Rigidbody _rb;
+
+    private Vector3 move;
 
     // Start is called before the first frame update
     void Start()
@@ -13,15 +30,36 @@ public class PlayerMovement : MonoBehaviour
         _rb = GetComponent<Rigidbody>();
     }
 
+    void Update()
+    {
+        if (Input.GetAxisRaw("Drift") > 0)
+        {
+            Traction = 1.0f;
+            left.emitting = true;
+            right.emitting = true;
+        }
+        else
+        {
+            Traction = 10.0f;
+            left.emitting = false;
+            right.emitting = false;
+        }
+    }
+
     void FixedUpdate()
     {
         var horizontal = Input.GetAxis("Horizontal");
         var vertical = Input.GetAxis("Vertical");
 
-        Camera cam = Camera.main;
+        _rb.position += move * Time.deltaTime;
 
-        Vector3 move = cam.transform.right * horizontal + cam.transform.forward * vertical;
+        move += _rb.transform.forward * vertical * MoveSpeed * Time.deltaTime;
+        move *= Drag;
+        move = Vector3.ClampMagnitude(move, MaxSpeed);
 
-        _rb.AddForce(move);
+        if(move.magnitude > 0)
+            _rb.transform.Rotate(Vector3.up * horizontal * SteerAngle * Time.deltaTime);
+        
+        move = Vector3.Lerp(move.normalized, transform.forward, Traction * Time.deltaTime) * move.magnitude;
     }
 }
